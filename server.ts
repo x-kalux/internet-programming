303a0f8a039d760e5cf953a0b6299db068482960
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2022-12-11 14:13:33 +0700

    api example

--- server.ts
+++ server.ts
@@ -1,31 +1,50 @@
-import express, { Express, Request, Response } from 'express'
-import path from 'path'
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import https from 'node:https';
+import { IncomingMessage } from 'node:http';
+import path from 'node:path';
+
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
+const apiKey = process.env.APIKEY
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-    const file = path.join(__dirname, '..', '/bmi.html')
-    res.sendFile(file)
+app.get('/', (request: express.Request, response: express.Response) => {
+    response.sendFile(path.join(__dirname, '..', '/src/index.html'))
 })
 
-app.post('/', (req: Request, res: Response) => {
-    const weight = Number(req.body.weight)
-    const height = Number(req.body.height)
-    const bmi = weight / height * height
-    res.send('BMI = ' + bmi)
-})
+app.post('/', (request: express.Request, response: express.Response) => {
+    const query = request.body.query
+    const unit = 'metric'
+    const endpoint = 'https://api.openweathermap.org/data/2.5/weather'
+    const url = `${endpoint}?q=${query}&units=${unit}&appid=${apiKey}`
+    https.get(url, (res: IncomingMessage) => {
+        console.log(res.statusCode)
+        res.on('data', (data) => {
+            const resData = JSON.parse(data)
+            console.log(resData)
+            if (resData.cod === '404') {
+                response.write(`<h1>Search(city) : ${query}</h1>`)
+                response.write(`<h2>${resData.message}</h2>`)
+            } else {
+                response.write(`<h1>The weather in ${resData.name} </h1>`)
+                response.write(`<h2>${resData.weather[0].main} (temp ${resData.main.temp} degrees)</h2>`)
 
-app.get('/aboutme', (req: Request, res: Response) => {
-    res.send('xkalux')
+            }
+            response.write(`<a href='/'>Back</a>`)
+            response.send()
+        })
+    })
 })
+// const query = 'nakhon ratchasima, th'
 
 app.listen(port, () => {
-    console.log(`⚡️[SERVER]: Server is running at https://localhost:${port}`)
+    console.log(`⚡️[SERVER]: Server is running on port: ${port}`)
 })
+
+
