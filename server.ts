cf008e037d63dbbeda3e57134cc4c6601ccc2831
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2022-12-13 17:53:34 +0700

    example

--- server.ts
+++ server.ts
@@ -1,29 +1,51 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import path from 'path'
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 
+const date = require(__dirname + '/date.js')
 dotenv.config()
-
-const app = express()
 const port = process.env.PORT
 
+const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+// const dateOptions: Intl.DateTimeFormatOptions = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric'
+// }
+// const today = new Date().toLocaleDateString('th-TH', dateOptions);
+const today = date.getDateNow()
+
+const tasks = new Set()
+const schoolTask = new Set()
 
 app.get('/', (req: Request, res: Response) => {
-    const file = path.join(__dirname, '..', '/bmi.html')
-    res.sendFile(file)
+    res.render('index', { taskTitle: 'General', today: today, tasks: tasks });
+})
+app.get('/school', (req: Request, res: Response) => {
+    res.render('index', { taskTitle: 'School', today: today, tasks: schoolTask });
 })
 
 app.post('/', (req: Request, res: Response) => {
-    const weight = Number(req.body.weight)
-    const height = Number(req.body.height)
-    const bmi = weight / height * height
-    res.send('BMI = ' + bmi)
-})
-
-app.get('/aboutme', (req: Request, res: Response) => {
-    res.send('xkalux')
+    let _tasks = tasks
+    let path = '/'
+    if (req.body.type === 'School') {
+        _tasks = schoolTask
+        path = 'school'
+    }
+    let newTask = req.body.newTask
+    if (newTask !== '') {
+        _tasks.add(newTask)
+    }
+    if (req.body.delete !== undefined) {
+        _tasks.delete(req.body.delete)
+    }
+    res.redirect(path)
 })
 
 app.listen(port, () => {
