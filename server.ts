741e290e6e73a61e97954354c2b4dbd80c1c41c5
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2022-12-20 09:04:30 +0700

    css

--- server.ts
+++ server.ts
@@ -1,20 +1,34 @@
 import express, { Express, Request, Response } from 'express'
-import { IncomingMessage } from 'http';
-import path from 'path'
+
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
-const https = require('https');
+
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
-const appid = process.env.APIKEY
-app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+const dateOption: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+}
+const toDay = new Date().toLocaleDateString('th-TH', dateOption);
+const todoList = new Set()
 app.get('/', (request: Request, response: Response) => {
-    response.send('Hello World')
+    response.render('index', { today: toDay, tasks: todoList })
 })
 
+app.post('/', (request: Request, response: Response) => {
+    const newTask = request.body.newTask
+    todoList.add(newTask)
+    response.redirect('/')
+})
 
 app.listen(port, () => {
     console.log(`⚡️[SERVER]: Server is running at https://localhost:${port}`)
