06d6aaefd850327ae31586d342b881d029b2f00c
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2023-01-17 15:50:10 +0700

    complete-todo

--- server.ts
+++ server.ts
@@ -1,43 +1,84 @@
 import express, { Express, Request, Response } from 'express'
+import { Schema, Document } from 'mongoose';//<----
 const date = require(__dirname + '/date.js');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 
 dotenv.config()
 
+const mongoose = require('mongoose') //<----
+mongoose.set('strictQuery', true)   //<----
+mongoose.connect(process.env.MONGODB_URL); //<----
+interface ITodo extends Document { //<----
+    task: String,
+    isDone: Boolean,
+    taskTitle: String,
+    date: Date
+}
+const todoSchema = new Schema({ //<----
+    task: String,
+    isDone: Boolean,
+    taskTitle: String,
+    date: { type: Date, default: Date.now }
+})
+const Todo = mongoose.model('Todo', todoSchema) //<----
+
 const app = express()
 const port = process.env.PORT
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 
 const toDay = date.getDateToDay();
-const todoHome = new Set()
+//const todoHome = new Set()
 const todoSchool = new Set()
 app.get('/', (request: Request, response: Response) => {
-    response.render('index', { today: toDay, tasks: todoHome, taskTitle: "Home" })
+    Todo.find({ taskTitle: 'Home' }, (err: string, todoHome: ITodo[]) => {
+        if (err)
+            response.send(err)
+        else
+            response.render('index', { today: toDay, tasks: todoHome, taskTitle: "Home" })
+    })
+
 })
 app.get('/school', (request: Request, response: Response) => {
-    response.render('index', { today: toDay, tasks: todoSchool, taskTitle: "School" })
+    Todo.find({ taskTitle: 'School' }, (err: string, todoHome: ITodo[]) => {
+        if (err)
+            response.send(err)
+        else
+            response.render('index', { today: toDay, tasks: todoHome, taskTitle: "School" })
+    })
 })
 
 app.post('/', (request: Request, response: Response) => {
-    let todoList = todoHome
+
     let path = '/'
     const newTask = request.body.newTask
+    const taskType = request.body.type
     if (request.body.type === 'School') {
-        todoList = todoSchool
         path = 'school'
     }
     if (newTask !== '') {
-        todoList.add(newTask)
+        const task = new Todo({
+            task: newTask,
+            isDone: false,
+            taskTitle: taskType
+        })
+        task.save()
+        response.redirect(path)
     }
     if (request.body.delete !== undefined) {
-        todoList.delete(request.body.delete)
+        const delete_id = request.body.delete
+        Todo.findByIdAndDelete(delete_id, (err: string) => {
+            if (err) {
+                response.send(err)
+            } else {
+                response.redirect(path)
+            }
+        })
     }
-    response.redirect(path)
+
 })
 
 app.listen(port, () => {
