476bc94a6b64dbbcc79dfe30f6728188c6ac02a1
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2023-01-17 14:39:48 +0700

    mongoose example

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express'
 import { IncomingMessage } from 'http';
+import { Mongoose, Schema } from 'mongoose';
 import path from 'path'
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
@@ -10,10 +11,63 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 const password = process.env.MONGODB_PASSWORD
-const mongodb_url = `mongodb+srv://xkaluxchimjan:${password}@cluster0.iwiplux.mongodb.net/?retryWrites=true&w=majority`
+const dbname = process.env.MONGODB_DB
+const mongodb_url = `mongodb+srv://xkaluxchimjan:${password}@cluster0.iwiplux.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', true)
 mongoose.connect(mongodb_url);
+
+interface ITodo {
+    task: String,
+    isDone: Boolean,
+    date: Date
+}
+const todoSchema = new Schema({
+    task: String,
+    isDone: Boolean,
+    date: { type: Date, default: Date.now }
+})
+const Todo = mongoose.model('Todo', todoSchema)
+// const gotoSchool = new Todo({
+//     task: "goto School",
+//     isDone: true
+// })
+// const buyMilk = new Todo({
+//     task: "buy milk",
+//     isDone: false
+// })
+// Todo.insertMany([gotoSchool, buyMilk], (err: string) => {
+//     if (err)
+//         console.log(err)
+//     else
+//         console.log('data saved.')
+// })
+// todo.save()
+
+// Todo.updateOne({ task: 'test' }, { task: 'test example' }, (err: string) => {
+//     if (err)
+//         console.log(err)
+//     else {
+//         console.log('succesfully updated.')
+//     }
+// })
+
+Todo.deleteOne({ task: 'test example' }, (err: string) => {
+    if (err)
+        console.log(err)
+    else
+        console.log('delete test example succesfuly')
+})
+
+// Todo.find(function (err: string, todos: ITodo[]) {
+//     if (err)
+//         console.log(err)
+//     else {
+//         todos.forEach((item) => {
+//             console.log(`${item.task} : ${item.isDone} ${item.date}`)
+//         })
+//     }
+// })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
