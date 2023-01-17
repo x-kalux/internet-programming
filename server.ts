2b867fca619fdd20e34babb2138d123342814dcc
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2023-01-17 16:16:35 +0700

    todo

--- server.ts
+++ server.ts
@@ -25,7 +25,6 @@ const Todo = mongoose.model('Todo', todoSchema) //<----
 
 const app = express()
 const port = process.env.PORT
-const appid = process.env.APIKEY
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -35,46 +34,52 @@ const toDay = date.getDateToDay();
 //const todoHome = new Set()
 const todoSchool = new Set()
 app.get('/', (request: Request, response: Response) => {
-    const htmlFile = path.join(__dirname, '..', '/src/index.html')
-    response.sendFile(htmlFile)
+    Todo.find({ taskTitle: 'Home' }, (err: string, todoHome: ITodo[]) => {
+        if (err)
+            response.send(err)
+        else
+            response.render('index', { today: toDay, tasks: todoHome, taskTitle: "Home" })
+    })
+
 })
-
-app.post('/', (request: Request, response: Response) => {
-    const query = request.body.query
-    const unit = 'metric'
-    const lang = 'th'
-    const endpoint = 'https://api.openweathermap.org/data/2.5/weather'
-    const url = `${endpoint}?q=${query}&units=${unit}&lang=${lang}&appid=${appid}`
-    https.get(url, (res: IncomingMessage) => {
-        res.on('data', (data) => {
-            const jsonData = JSON.parse(data)
-            response.write(`<!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>My Fist WebApp</title>
-            </head><body>`)
-            if (jsonData.cod !== 200) {
-                response.write(`<h1>search(city): ${query}</h1>`)
-                response.write(`<h2>${jsonData.message}</h2>`)
-            } else {
-                response.write(`<h1>the weather in ${jsonData.name}</h1>`)
-                response.write(`<h2>${jsonData.weather[0].main}</h2>`)
-                response.write(`<h3>${jsonData.weather[0].description}</h3>`)
-            }
-            response.write('<a href="/">back</a>')
-            response.write('<body>')
-            console.log(jsonData)
-
-            response.send()
-        })
+app.get('/school', (request: Request, response: Response) => {
+    Todo.find({ taskTitle: 'School' }, (err: string, todoHome: ITodo[]) => {
+        if (err)
+            response.send(err)
+        else
+            response.render('index', { today: toDay, tasks: todoHome, taskTitle: "School" })
     })
 })
 
+app.post('/', (request: Request, response: Response) => {
 
+    let path = '/'
+    const newTask = request.body.newTask
+    const taskType = request.body.type
+    if (request.body.type === 'School') {
+        path = 'school'
+    }
+    if (newTask !== '') {
+        const task = new Todo({
+            task: newTask,
+            isDone: false,
+            taskTitle: taskType
+        })
+        task.save()
+        response.redirect(path)
+    }
+    if (request.body.delete !== undefined) {
+        const delete_id = request.body.delete
+        Todo.findByIdAndDelete(delete_id, (err: string) => {
+            if (err) {
+                response.send(err)
+            } else {
+                response.redirect(path)
+            }
+        })
+    }
+
+})
 
 app.listen(port, () => {
     console.log(`⚡️[SERVER]: Server is running at https://localhost:${port}`)
