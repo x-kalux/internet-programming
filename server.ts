a224d5ca709c1995da287db8432c8357effe9057
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2023-01-17 13:01:29 +0700

    nodejs+mongoos

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import { IncomingMessage } from 'http';
 import path from 'path'
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const https = require('https');
@@ -8,6 +9,11 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT
+const password = process.env.MONGODB_PASSWORD
+const mongodb_url = `mongodb+srv://xkaluxchimjan:${password}@cluster0.iwiplux.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', true)
+mongoose.connect(mongodb_url);
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
